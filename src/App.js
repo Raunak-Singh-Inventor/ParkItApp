a6bb334d75a7c5d4b3e078415f195f6ae89edaa4
Raunak Singh <Raunak.singh.inventor@gmail.com>
2021-07-26 15:50:38 -0400

    added logo and and title tab of website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import logo from '../src/images/Logo.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row">
+        <div className="col-md">
+          <img style={{height:100,width:100}} src={logo} alt={logo}/>
+        </div>
+      </div>
     </div>
   );
 }
