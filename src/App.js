b78e78d3c4e33986cb63a98691f0b721729ce3cd
Raunak Singh <Raunak.singh.inventor@gmail.com>
2021-07-26 17:59:27 -0400

    added Park It title and mockep up Device ID

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-import logo from '../src/images/Logo.jpg';
+import { Avatar } from "react-rainbow-components";
+
+import logo from "../src/images/Logo.jpg";
 
 function App() {
   return (
     <div className="App">
       <div className="row">
-        <div className="col-md">
-          <img style={{height:100,width:100}} src={logo} alt={logo}/>
+        <div className="col-md-4">
+          <h1 style={{ color: "white", padding: 20 }}>Park It!</h1>
+        </div>
+        <div className="col-md-4">
+          <img style={{ height: 100, width: 100 }} src={logo} alt={logo} />
+        </div>
+        <div className="col-md-4">
+          <h1 style={{ color: "white", padding: 20 }}>Device ID: 12345</h1>
         </div>
       </div>
     </div>
