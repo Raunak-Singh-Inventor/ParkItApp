250fce465fe6c6be38b2334c0796d723ccbb4799
Raunak-Singh-Inventor <raunak.singh.inventor@gmail.com>
2021-08-11 18:58:55 -0400

    centered sign up component

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -9,8 +9,8 @@ import SubmitButton from "../SubmitButton";
 export default function SignUp(props) {
   const [documentHeight, setDocumentHeight] = useState(0);
   const [cardMargintop, setCardMarginTop] = useState(300);
-  const [cardHeight, setCardHeight] = useState(400);
-  const [signInGirlImageSize, setSignInGirlImageSize] = useState(400);
+  const [cardHeight, setCardHeight] = useState(550);
+  const [signInGirlImageSize, setSignInGirlImageSize] = useState(500);
   // eslint-disable-next-line
   useEffect(() => {
     setDocumentHeight(document.documentElement.offsetHeight);
@@ -28,16 +28,16 @@ export default function SignUp(props) {
   useEffect(() => {
     if (isTabletOrMobile === true && isPortrait === true) {
       setCardMarginTop(documentHeight / 2);
-      setCardHeight(700);
+      setCardHeight(1000);
       setSignInGirlImageSize(300);
     } else if (isTabletOrMobile === true && isPortrait === false) {
       setCardMarginTop(documentHeight / 2);
-      setCardHeight(400);
-      setSignInGirlImageSize(400);
+      setCardHeight(550);
+      setSignInGirlImageSize(300);
     } else {
       setCardMarginTop(300);
-      setCardHeight(400);
-      setSignInGirlImageSize(400);
+      setCardHeight(550);
+      setSignInGirlImageSize(500);
     }
   });
 
@@ -51,7 +51,7 @@ export default function SignUp(props) {
             style={{
               textAlign: "center",
               marginTop: cardMargintop,
-              width: 700,
+              width: 2000,
               height: cardHeight,
             }}
           >
@@ -60,28 +60,36 @@ export default function SignUp(props) {
             </h1>
             <div className="row">
               <div className="col-md-6">
-                <img style={{ width: signInGirlImageSize }} src={signingirl} alt={"signIn"}/>
+                <img
+                  style={{ width: signInGirlImageSize }}
+                  src={signingirl}
+                  alt={"signIn"}
+                />
               </div>
               <div className="col-md-6">
                 <form noValidate autoComplete="off">
                   <div>
-                    <TextField
-                      style={{ marginTop: 20, marginRight: 10 }}
-                      onChange={props.onChange}
-                      label="username"
-                      name="username"
-                      error={props.isUsernameError}
-                      required
-                    />
-                    <TextField
-                      style={{ marginTop: 20 }}
-                      onChange={props.onChange}
-                      label="password"
-                      name="password"
-                      type="password"
-                      error={props.isPasswordError}
-                      required
-                    />
+                    <div>
+                      <TextField
+                        style={{ marginTop: 20, width:500 }}
+                        onChange={props.onChange}
+                        label="username"
+                        name="username"
+                        error={props.isUsernameError}
+                        required
+                      />
+                    </div>
+                    <div>
+                      <TextField
+                        style={{ marginTop: 20, width:500  }}
+                        onChange={props.onChange}
+                        label="password"
+                        name="password"
+                        type="password"
+                        error={props.isPasswordError}
+                        required
+                      />
+                    </div>
                     <div style={{ marginTop: 20 }}>
                       <SubmitButton
                         username={props.username}
@@ -95,7 +103,7 @@ export default function SignUp(props) {
                       />
                     </div>
                     <div>
-                      <h6>or</h6>
+                      <h6>-----------------------------------------------or-----------------------------------------------</h6>
                     </div>
                     <div>
                       <Button onClick={props.createAccount}>
